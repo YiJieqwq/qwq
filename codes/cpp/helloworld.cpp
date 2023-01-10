5a53ca3ed938381ef0182b465179d7199ecf7387
YiJieqwq <114742583+YiJieqwq@users.noreply.github.com>
2023-01-10 11:23:54 +0800

    Add files via upload

---
+++ codes/cpp/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main(){
+    char name[10];
+    cout<<"Input your name: "<<endl;
+    cin>>name;
+    cout<<"Hello "<<name<<" this is your vscode!";
+    return 0;
+}
